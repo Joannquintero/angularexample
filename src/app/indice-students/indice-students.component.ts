ba2062bfff7dae6b4534bde69a30c7226c3f3bf5
joann.quintero <joann.quintero@kpgin.com>
2025-07-10 19:54:56 -0500

    clear code

--- src/app/indice-students/indice-students.component.ts
+++ src/app/indice-students/indice-students.component.ts
@@ -1,26 +1,25 @@
 import { Component, inject } from '@angular/core';
 import { IndiceStudentsService } from '../indice-students.service';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
 import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-indice-students',
   standalone: true,
-  imports: [ MatButtonModule, RouterLink, MatTableModule ],
+  imports: [MatButtonModule, MatTableModule],
   templateUrl: './indice-students.component.html',
-  styleUrl: './indice-students.component.css'
+  styleUrl: './indice-students.component.css',
 })
 export class IndiceStudentsComponent {
   displayedColumns: string[] = ['estudiante', 'email', 'action'];
-  dataSource: any[] = []
+  dataSource: any[] = [];
 
- coursesServices = inject(IndiceStudentsService);
+  coursesServices = inject(IndiceStudentsService);
   students: any[] = [];
-    constructor(){
-    this.coursesServices.getCourses().subscribe(response => {
+  constructor() {
+    this.coursesServices.getCourses().subscribe((response) => {
       this.students = response;
       this.dataSource = response;
     });
-  } 
+  }
 }
