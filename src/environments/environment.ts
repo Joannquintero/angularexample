6ccb583436dceec0e86bf1b54c287e02bdaaf3f1
gabijoann <gabijoann@hotmail.com>
2025-07-14 15:50:36 -0500

    aumentar tamano maximumWarning deploy aws

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   apiURL: 'http://sitepruebatecinterdomain.somee.com',
-  urlLocal: 'http://localhost:4200',
+  urlLocal: 'http://joannqyomailir.us-east-2.elasticbeanstalk.com',
 };
